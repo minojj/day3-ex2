876531b8abf236b154a91ae36b4d83d90e60b840
csk-qa <sndkqkfk7@gmail.com>
2025-10-30 15:14:40 +0900

    chore: 3만 고려하도록 수정

--- app2.py
+++ app2.py
@@ -3,7 +3,7 @@ for i in range(100, 0, -1):
     count = 0
 
     for j in str(i):
-        count += 1 if j in '369' else 0
+        count += 1 if j in '3' else 0
 
     if count == 0:
         print(i)
