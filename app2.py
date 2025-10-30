9e436e2b828fec99bc5e4fe3b6839af96b796e50
minojj <wrnx8vzkyz@privaterelay.appleid.com>
2025-10-30 15:15:20 +0900

    Update app2.py

---
+++ app2.py
@@ -0,0 +1,13 @@
+for i in range(100, 0, -1):
+    
+    num_str = str(i)
+    
+    clap = 0
+    for digit in num_str:
+        if digit in ('3', '6', '9'):
+            clap = clap + 1
+            
+    if clap > 0:
+        print("짝" * clap)
+    else:
+        print(i)
