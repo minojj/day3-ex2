6e16592c1c88a135f09078cc9c06a55a02bfcb66
csk-qa <sndkqkfk7@gmail.com>
2025-10-30 15:12:41 +0900

    feat: 369 게임

---
+++ app2.py
@@ -0,0 +1,15 @@
+
+for i in range(100, 0, -1):
+    count = 0
+
+    for j in str(i):
+        count += 1 if j in '369' else 0
+
+    if count == 0:
+        print(i)
+        continue
+
+    for j in range(count):
+        print('짝', end="")
+
+    print()
